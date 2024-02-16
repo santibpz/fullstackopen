9b3fe9307dc1bf762f8569630e2bc7bc9715aa29
Santiago Benitez <A01782813@tec.mx>
2024-02-16 10:20:24 -0600

    completed exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,11 +7,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   // handler functions
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+  
+  const handleNeutralClick = () =>  {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+    
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+
+  } 
 
   return (
     <div>
@@ -28,6 +41,9 @@ const App = () => {
       <Stat category="good" stat = {good} />
       <Stat category="neutral" stat = {neutral} />
       <Stat category="bad" stat = {bad} />
+      <Stat category="all" stat = {total} /> 
+      <Stat category="average" stat = {total == 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total} /> 
+      <Stat category="average" stat = {total == 0 ? 0 : `${(good * 100) / total} %`} /> 
     </div>
   )
 }
