9c3a18e8598374b6038b19439d6f2834516dea00
Santiago Benitez <A01782813@tec.mx>
2024-02-19 23:26:08 -0600

    completed exercise 2.2

--- part2/courseinfo_continuation/src/Components/Course.jsx
+++ part2/courseinfo_continuation/src/Components/Course.jsx
@@ -2,12 +2,14 @@
 
 import Content from "./Content"
 import Header from "./Header"
+import Total from "./Total"
 
 const Course = ({ course }) => {
   return (
     <>
       <Header course={course.name} />
       <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </>
   )
 }
--- part2/courseinfo_continuation/src/Components/Total.jsx
+++ part2/courseinfo_continuation/src/Components/Total.jsx
@@ -1,6 +1,6 @@
 
 const Total = ({parts}) => {
-  return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  return <p><b>total of {parts.map(part => part.exercises).reduce((acc, current) => acc + current)} exercises</b></p>
 }
 
 export default Total
